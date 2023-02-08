3dbe35a80c77f332aa35eabddbced458b4a726df
Jose Valim <joseaugustovalim1994@gmail.com>
2023-02-08 12:03:24 -0300

    feature:"Add the list of play the sound"

--- main.js
+++ main.js
@@ -2,4 +2,9 @@ function toca_acaba() {
   document.querySelector('#tecla_acaba').play();
 }
 
-document.querySelector('.tecla_acaba').onclick = toca_acaba;
+//document.querySelector('.tecla_acaba').onclick = toca_acaba;
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick=toca_acaba();
+
