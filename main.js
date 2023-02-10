cbca8ab68d88fc4b05f92419af8c55d990a568c3
Jose Valim <joseaugustovalim1994@gmail.com>
2023-02-10 14:50:20 -0300

    feature: Add a background  cover image

--- main.js
+++ main.js
@@ -4,18 +4,17 @@ function tocaaaaaaa(idElementoAudio) {
 
 //document.querySelector('.tecla_acaba').onclick = tocaaaaaaa;
 
+//Aqui crio o acesso a todos os elementos com a classe tecla
 const listaDeTeclas = document.querySelectorAll('.tecla');
+//document.querySelector retorna todos os elementos de um seletor especifico 
 
-let i = 0;
 
-while (i < listaDeTeclas.length) {
-  const tecla = listaDeTeclas[i];
-  const instrumento = tecla.classList[1];
-  const idAudio = `#som_${instrumento}`; //template string
+for (let i = 0; i < listaDeTeclas.length; i++) { //Este for faz um percurso do tamanho da lista de teclas
+  const tecla = listaDeTeclas[i]; //Esta variavel esta aqui para que consiga se acessar o elemento de uma determinada posição
+  const instrumento = tecla.classList[1]; //Aqui acessamos a segunda classe que difere da primeira
+  const idAudio = `#som_${instrumento}`; //template string Criamos o seletor com o id vindo da variavel instrumento
 
-  tecla.onclick = function () {
-    tocaaaaaaa(idAudio);
+  tecla.onclick = function () { //Quando o elemento tecla tiver a ação de clique
+    tocaaaaaaa(idAudio); //chamamos a função toca passando para ela o seletor fonecido pela variavel const idAudio
   };
-
-  i += 1;
 }
